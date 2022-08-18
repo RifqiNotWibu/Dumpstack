297e9069a5b6a1a6fc6e9f4e179e6b470c4b46a8
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-08-18 21:01:04 +0700

    Coret coret DOM selection

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//test
+const judul = document.getElementById("judul"); //Return --> Element
+//Styling modify test
+judul.style.color = "blue";
+judul.style.backgroundColor = "yellow";
+judul.innerHTML = "JS DOM TEST";
+
+const p = document.getElementsByTagName("p"); //Return --> Array
+//Styling modify test
+for (i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = "yellow"; //Bulk recoloring
+}
