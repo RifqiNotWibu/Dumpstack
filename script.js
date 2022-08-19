da0298518b0b4ab782c13733eccd4f3ca8a8d920
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-08-20 05:56:54 +0700

    Testing out querySelector

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ const p = document.getElementsByTagName("p"); //Return --> Array
 for (i = 0; i < p.length; i++) {
   p[i].style.backgroundColor = "yellow"; //Bulk recoloring
 }
+
+const p1 = document.getElementsByClassName("p1"); //Return --> Element
+//Styling modify test
+p1.innerHTML = "Paragraf ini telah diubah";
+
+const p4 = document.querySelector("#b p"); //Selecting p class in section with id = b
+//Styling modify test
+p4.style.backgroundColor = "green";
+
+const li2 = document.querySelector("section#b ul li:nth-child(2)");
+//Styling modify test
+li2.innerHTML = "Paragraf ini telah diubah melalui querySelector yang spesifik";
