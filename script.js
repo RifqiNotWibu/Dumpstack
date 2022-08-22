722f23a4d6c6c033fe621de542daa659215b1b0a
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-08-23 04:17:36 +0700

    Document manipulating

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ judul.style.color = "blue";
 judul.style.backgroundColor = "yellow";
 judul.innerHTML = "JS DOM TEST";
 
-const p = document.getElementsByTagName("p"); //Return --> Array
+const p = document.getElementsByTagName("p")[0]; //Return --> Array
 //Styling modify test
 for (i = 0; i < p.length; i++) {
   p[i].style.backgroundColor = "yellow"; //Bulk recoloring
@@ -21,3 +21,23 @@ p4.style.backgroundColor = "green";
 const li2 = document.querySelector("section#b ul li:nth-child(2)");
 //Styling modify test
 li2.innerHTML = "Paragraf ini telah diubah melalui querySelector yang spesifik";
+
+const qsp = document.querySelectorAll("p"); //Return --> Nodelist(array)
+//Styling modify test
+qsp[1].style.backgroundColor = "lightblue";
+
+p.setAttribute("name", "Rifqi"); //Sets "p" - "name" attribute to "Rifqi"
+p.getAttribute("name"); //Sees the attribute mentioned (ENTER @ CONSOLE)
+p.removeAttribute("name"); //Removes the attribute mentioned (ENTER @ CONSOLE)
+
+//Creating new element
+const newP = document.createElement("p");
+const textP = document.createTextNode(
+  "Ini adalah paragraf yang baru ditambahkan"
+);
+
+newP.appendChild(textP); //Appending created text into the paragraph
+
+//Putting newP into a specified section in the document
+const sectionA = document.getElementById("a");
+sectionA.appendChild(newP);
